633882bc6d932bd8ccadf977de9e5b0314222b62
Daniel Squires <squiresstewart@icloud.com>
2021-11-04 14:33:41 -0500

    bullshit

--- GeoJSON_LineStrings/static/js/logic.js
+++ GeoJSON_LineStrings/static/js/logic.js
@@ -2,29 +2,7 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
+let map = L.map('mapid').setView([44.0, -80.0], 10)
 
 L.geoJson(sanFranAirport, {
     // We turn each feature into a marker on the map.
@@ -36,14 +14,11 @@ L.geoJson(sanFranAirport, {
 
   }).addTo(map);
 
-// L.geoJson(data, {
-//     onEachFeature: function(feature, layer) {
-//       layer.bindPopup();
-//      }
-// });
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/squiresstewart/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     
