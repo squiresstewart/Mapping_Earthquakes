9c7dfa6b365686918f4d15b3e4cc1f3744261367
Daniel Squires <squiresstewart@icloud.com>
2021-11-05 19:39:40 -0500

    done

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -28,7 +28,8 @@ let earthquakes = new L.layerGroup();
 // We define an object that contains the overlays.
 // This overlay will be visible all the time.
 let overlays = {
-  Earthquakes: earthquakes
+  Earthquakes: earthquakes,
+  layer: [streets]
 };
 
 
@@ -38,8 +39,9 @@ let overlays = {
 let map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
-    layer: [streets]
-});
+    layers: [streets, earthquakes]
+
+  });
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
